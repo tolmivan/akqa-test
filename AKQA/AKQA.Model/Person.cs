4901ff8097d96fcda8f5c45450287fbd87b8f7fb
Ivan Tolmachev <Ivan.X.Tolmachev@mlcinsurance.com.au>
2020-01-10 19:51:14 +1100

    a bit of front-end before dinner

---
+++ AKQA/AKQA.Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AKQA.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string HumanizedNumber { get; set; }
+    }
+}
---
+++ AKQA/AKQA.WebApi/Controllers/HumanizerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AKQA.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKQA.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HumanizerController : ControllerBase
+    {
+        // POST api/humanizer
+        [HttpPost]
+        public ActionResult<Person> Post(Person value)
+        {
+            value.HumanizedNumber = "ONE HUNDRED AND TWENTY-THREE DOLLARS AND FORTY-FIVE CENTS";
+            return value;
+        }
+    }
+}
